ac4ae0f954af59d5f1cce2bffe51ad942c2668d2
jmhIcoding <1024836002@qq.com>
2019-04-27 11:26:42 +0800

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -1,1 +1,18 @@
-
+#using:utf-8
+__author__ = 'dk'
+import  json
+num_max =5000
+r=3
+d=2*r
+point=[]
+delta= d/num_max
+x=-r
+while len(point) < 2*num_max:
+    x=round(x,8)
+    y=(r**2 - x**2)**0.5
+    y= round(y,8)
+    point.append([x,y])
+    point.append([x,-y])
+    x+=delta
+print(len(point))
+print(point)
